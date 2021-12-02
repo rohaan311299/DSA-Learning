9b9bd718c7aa78cd47af3c047c7d1144fec8328e
Rohan Kacheria <rohankacheriam@gmail.com>
2021-12-02 08:54:21 +0530

    functions

---
+++ section2_essential_concepts/functions/functions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int add(int a, int b)
+{
+    int c;
+    c = a + b;
+    return c;
+}
+
+int main()
+{
+    int x, y, z;
+    x = 10, y = 15;
+    z = add(x, y); // function calling
+    cout << z << endl;
+
+    return 0;
+}
